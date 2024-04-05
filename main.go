07adf01573a28d4a03fd866b716c7eba7c7e5db0
BrotherSam66 <112906505@qq.com>
2024-04-05 10:23:51 +0800

    all

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 
 // this is main func.
 func main() {
-	stopAllCh := make(chan struct{}) // If close this chanel all goroutine will cancel.
+	stopAllCh := make(chan struct{}) // If  close this chanel all goroutine will cancel.
 	sub.Sub(stopAllCh)
 	fmt.Println("sub() 回来了") // do it
 	time.Sleep(time.Millisecond * 200000)
